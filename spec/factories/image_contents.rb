cc0dcbd8858309045abdde81d55f682d022fe069
Giyas Guliyev <g.g.giyas@gmail.com>
2018-01-23 15:57:43 +0300

    Image Content Data Model added

--- spec/factories/image_contents.rb
+++ spec/factories/image_contents.rb
@@ -1,10 +1,14 @@
+require_relative '../support/image_content_helper.rb'
 FactoryBot.define do
   factory :image_content do
-    image_id 1
-    width 1
-    height 1
-    content_type "MyString"
-    content ""
-    original false
+    content_type "image/jpg"
+        #default to the original size when we generate just attributes
+    sequence(:content) {|idx|
+      File.open(ImageContentHelper.sample_filepath,"rb") { |f|
+        image=StringIO.new(f.read)
+        image=ImageContentCreator.annotate(idx, image)
+        Base64.encode64(image)
+      }
+    }
   end
 end
